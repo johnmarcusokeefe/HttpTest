69af57cb1005e3428df7ceaaa5bf07ceb55d5332
Johno <30913052+johnmarcusokeefe@users.noreply.github.com>
2022-12-17 13:09:55 +1100

    blockview implementation ongoing

--- app/src/main/java/com/example/httptest/BlockControl.java
+++ app/src/main/java/com/example/httptest/BlockControl.java
@@ -2,8 +2,11 @@ package com.example.httptest;
 
 public class BlockControl {
 
+    ViewAdapter viewAdapter = new ViewAdapter();
+
     String url = "https://okka.com.au/driverlog_rest/blocks.php";
     HttpStreamControl hsc = new HttpStreamControl();
+
 
     Boolean flag = hsc.httpRequest(url);
 
--- app/src/main/java/com/example/httptest/BlockView.java
+++ app/src/main/java/com/example/httptest/BlockView.java
@@ -2,7 +2,10 @@ package com.example.httptest;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -10,16 +13,21 @@ import java.util.ArrayList;
 
 public class BlockView extends OptionsMenu {
 
+    // initiate row variables
+    TextView blockId;
+    TextView blockDate;
+    TextView blockRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_block_view);
+        //setContentView(R.layout.activity_block_view);
+
+
 
         BlockControl bc = new BlockControl();
 
-
         if(bc.flag){
-
              // may have to check if returned
             JSONObject r = HttpStreamControl.getJsonResult();
             System.out.println("blockview flag = true ");
@@ -46,10 +54,19 @@ public class BlockView extends OptionsMenu {
                 }
 
                 System.out.println("arraylist blockview "+ al);
-                ArrayAdapter<String> listAdapter =
-                        new ArrayAdapter<>(this, R.layout.simplerow, al);
-                ListView blocklist = findViewById(R.id.block_list);
-                blocklist.setAdapter(listAdapter);
+
+                blockId = findViewById(R.id.block_id);
+                blockDate = findViewById(R.id.block_date);
+                blockRef = findViewById(R.id.block_ref);
+
+                
+
+//                ArrayAdapter<String> listAdapter =
+//                        new ArrayAdapter<>(this, R.layout.activity_block_row, al);
+//
+//
+//                ListView blocklist = findViewById(R.id.block_list);
+                //blocklist.setAdapter(listAdapter);
                 
             } catch (JSONException e) {
                 e.printStackTrace();
