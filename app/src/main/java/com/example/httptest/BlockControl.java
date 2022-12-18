e00a1179d74dc08c21fd9922e7de277400de3873
Johno <30913052+johnmarcusokeefe@users.noreply.github.com>
2022-12-18 13:18:33 +1100

    blockview implementation ongoing

--- app/src/main/java/com/example/httptest/BlockControl.java
+++ app/src/main/java/com/example/httptest/BlockControl.java
@@ -2,7 +2,6 @@ package com.example.httptest;
 
 public class BlockControl {
 
-    ViewAdapter viewAdapter = new ViewAdapter();
 
     String url = "https://okka.com.au/driverlog_rest/blocks.php";
     HttpStreamControl hsc = new HttpStreamControl();
