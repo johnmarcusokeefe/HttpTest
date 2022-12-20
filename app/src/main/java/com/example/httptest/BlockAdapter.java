8d64d5d9b93b57515f3f57674f74bd919ba7f901
John O'Keefe <johnmarcusokeefe@gmail.com>
2022-12-21 09:52:54 +1100

    blockadapter code

--- app/src/main/java/com/example/httptest/BlockAdapter.java
+++ app/src/main/java/com/example/httptest/BlockAdapter.java
@@ -8,17 +8,34 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BlockAdapter extends ArrayAdapter<Block> {
 
-    //private String[] localDataSet;
-    private List<Block> localDataSet;
+
+    public BlockAdapter(@NonNull Context context, ArrayList<Block> block) {
+        super(context, 0, block);
+    }
+
+    public View getView(int position, @Nullable View viewItem, @NonNull ViewGroup viewGroup) {
+
+        if(viewItem==null){
+            Block block = getItem(position);
+
+            TextView id = viewItem.findViewById(R.id.block_id);
+            TextView date = viewItem.findViewById(R.id.block_date);
+            TextView job = viewItem.findViewById(R.id.block_job);
+
+            id.setText(block.getId());
+            date.setText(block.getDate());
+            job.setText(block.getJob());
+        }
 
 
-    public BlockAdapter(@NonNull Context context, int resource) {
-        super(context, resource);
+        return viewItem;
     }
 }
