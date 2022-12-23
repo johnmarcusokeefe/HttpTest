e086c046607df671e4e8f2e7cee3f8543a00aabc
Johno <30913052+johnmarcusokeefe@users.noreply.github.com>
2022-12-24 09:38:39 +1100

    removal of resultparser from httpstreamcontrol

--- app/src/main/java/com/example/httptest/HttpStreamControl.java
+++ app/src/main/java/com/example/httptest/HttpStreamControl.java
@@ -2,7 +2,9 @@ package com.example.httptest;
 
 import org.json.JSONObject;
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -30,16 +32,15 @@ public class HttpStreamControl extends Thread {
         url_string = url_in;
         HttpStreamControl thread = new HttpStreamControl();
         thread.start();
-
-        while(thread.isAlive()){
-            System.out.println("getting data");
-        }
+//        while(thread.isAlive()){
+//            System.out.println("getting data");
+//        }
+        System.out.println("exit while");
         return true;
     }
 
     public void run() {
         try {
-            System.out.println("run "+url_string);
             URL url = new URL(url_string);
             httpConnection = (HttpsURLConnection) url.openConnection();
             httpConnection.setRequestMethod("POST");
@@ -50,16 +51,21 @@ public class HttpStreamControl extends Thread {
             httpConnection.getOutputStream().close();
             // get response
             InputStream in = new BufferedInputStream(httpConnection.getInputStream());
-            // parses the data
-            lr = new ResultParser();
-            JSONObject object = lr.resultParser(in);
-            setJsonResult(object);
+
+                BufferedReader rawJson = new BufferedReader(new InputStreamReader(in));
+                JSONObject object = new JSONObject(rawJson.readLine());
+                rawJson.close();
+                setJsonResult(object);
 
         } catch (Exception e) {
+
             e.printStackTrace();
+
         }
         finally {
             httpConnection.disconnect();
+            currentThread().interrupt();
+
         }
     }
 
--- app/src/main/java/com/example/httptest/ResultParser.java
+++ app/src/main/java/com/example/httptest/ResultParser.java
@@ -12,12 +12,10 @@ public class ResultParser {
     // result parser json
     public JSONObject resultParser(InputStream in) throws IOException, JSONException {
 
-        BufferedReader json
-                = new BufferedReader(new InputStreamReader(in));
-        //
+        BufferedReader json = new BufferedReader(new InputStreamReader(in));
         JSONObject object = new JSONObject(json.readLine());
-        System.out.println("object "+object);
         json.close();
+        
         return object;
     }
 
